5fd879c2366a9d4274ba3c69cb2fca9e778af7f0
juan pablo <bolinojuan@gmail.com>
2023-09-08 12:38:13 -0300

    modificacion entidad Alumno

--- Guia5/src/guia5/Entidades/Alumno.java
+++ Guia5/src/guia5/Entidades/Alumno.java
@@ -1,7 +1,15 @@
 
 package guia5.Entidades;
 
+import java.time.LocalDate;
+
 
 public class Alumno {
 
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fecha;
+    private boolean activo;
+    
 }
