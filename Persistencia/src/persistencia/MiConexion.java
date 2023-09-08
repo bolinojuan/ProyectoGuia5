baf8cb7f3fc3005ff33e33cebecb51c70a5b5f6f
juan pablo <82359005+bolinojuan@users.noreply.github.com>
2023-09-07 21:14:21 -0300

    MiConexion fue agregada al proyecto

---
+++ Persistencia/src/persistencia/MiConexion.java
@@ -0,0 +1,37 @@
+
+package persistencia;
+import java.sql.*;
+import javax.swing.*;
+
+public class MiConexion {
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String USER = "root";
+    private static final String DB = "universidadEjemplo";
+    private static final String PASSWORD = "";
+    private static Connection conexion;
+    
+    public Connection getConexion(){
+
+    if(conexion == null){
+    
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            conexion = DriverManager.getConnection(URL+DB+" ?useLegacyDatetimeCode=false&serverTimezone=UTC"+"&user="+ USER + "&password="+PASSWORD);
+            
+        } catch (ClassNotFoundException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al cargar el driver  "+ ex.getMessage());
+      
+        }catch(SQLException ex){
+     
+            JOptionPane.showMessageDialog(null,"Error de conexion a la BD " +ex.getMessage());
+        }
+    }
+    return conexion;
+    }
+
+    
+
+}
---
+++ Persistencia/src/persistencia/Persistencia.java
@@ -0,0 +1,11 @@
+
+package persistencia;
+
+
+public class Persistencia {
+
+    public static void main(String[] args) {
+     
+    }
+
+}
