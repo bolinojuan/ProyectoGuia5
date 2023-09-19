779ae7e526cba5c03d38fb638291f3af96839b4f
juan pablo <bolinojuan@gmail.com>
2023-09-19 09:22:49 -0300

    Update AlumnoData.java

--- Guia5/src/guia5/AccesoADatos/AlumnoData.java
+++ Guia5/src/guia5/AccesoADatos/AlumnoData.java
@@ -171,16 +171,14 @@ public class AlumnoData {
 
      public void modificarAlumno(Alumno alumno){
      
-         String modificar = "UPDATE alumno SET dni = ?, apellido = ?, nombre = ?,fechaNacimiento = ?, estado = ? WHERE dni= ? ";
+         String sql = "UPDATE alumno SET dni = ?, apellido = ?, nombre = ?,fechaNacimiento = ?, estado = ? WHERE dni= ? ";
          
 
 
         try {
-            PreparedStatement ps = con.prepareStatement(modificar);
+            PreparedStatement ps = con.prepareStatement(sql);
       
-            ps.setInt(1,alumno.getDni()); 
-            
-       
+            ps.setInt(1,alumno.getDni());       
             ps.setString(2,alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
