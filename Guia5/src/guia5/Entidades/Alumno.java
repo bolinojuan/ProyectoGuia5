556f4ca2bdde7c40bb0b4a22ca40a5a4c256669a
CarolinaOOrozco <carolinagorozco@gmail.com>
2023-09-23 12:39:39 -0300

    ...

--- Guia5/src/guia5/Entidades/Alumno.java
+++ Guia5/src/guia5/Entidades/Alumno.java
@@ -82,7 +82,7 @@ public class Alumno {
 
     @Override
     public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", activo=" + activo + '}';
+        return apellido +" " + nombre + " idAlumno=" + idAlumno + ", dni=" + dni + ", fechaNac=" + fechaNac + ", activo=" + activo;
     }
 
 
